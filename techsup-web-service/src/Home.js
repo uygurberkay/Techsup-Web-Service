6bb9ac51521f842a750677db2e8566b39ddc5499
uygurberkay <50241715+uygurberkay@users.noreply.github.com>
2023-01-09 20:12:01 +0300

    homePage-refactoring-fix

--- techsup-web-service/src/Home.js
+++ techsup-web-service/src/Home.js
@@ -12,7 +12,8 @@ function Home(){
                 <img className="home__image" src={require('./img/MainPage_Pt3.png')} />
                 <img className="home__image" src={require('./img/MainPage_Pt4.png')} />
                 <img className="home__image" src={require('./img/MainPage_Pt5.png')} /> */}
-            </div><div className="home__row" >
+            </div>
+            <div className="home__row" >
                 {/* Product */}
                 <Product title='ARC 770/Intel Core i5 12400F/INTEL Arc A770 16GB GDDR6/16GB DDR4/512GB M.2 NVMe SSD/INTEL ARC PC'
                 price={'19.000'} image ={require('./img/Product_Img1.png')} rating={4}/>
@@ -20,7 +21,8 @@ function Home(){
                 <Product title={'Lenovo Tab M10 HD Tablet 2. Nesil, 10.1" HD, MediaTek Helio P22T, 2.3 GHz, 4GB RAM, 64GB, Demir Gri, ZA6W0121TR'}
                 price ={'3.299'} image = {require('./img/Product_Img2.png')} rating={3}
                 />
-            </div><div className="home__row" >
+            </div>
+            <div className="home__row" >
                 {/* Product */}
                 <Product title={'Fluke 15B+ Dijital Multimetre'}
                 price = {'3.945,31'}  image = {require('./img/Product_Img3.png')}  rating={5}
@@ -32,7 +34,8 @@ function Home(){
                 <Product title={'Philips HD9255/60 Airfryer Fritöz, Uzaktan Bağlantı, 0.8 kg, 7 Ön Ayar'}
                 price = {'4.494'} image = {require('./img/Product_Img6.png')} rating={4}  
                 />
-            </div><div className="home__row" >
+            </div>
+            <div className="home__row" >
                 {/* Product */}
                 <Product title={'Gigabyte G32QC A oyun monitörü (32 inç, VA panel, 165 Hz, QHD çözünürlük, kavisli ekran)'}
                 price ={'7.035'} image = {require('./img/Product_Img5.png')} rating= {5}/> 
