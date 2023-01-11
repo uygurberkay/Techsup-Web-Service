2dd4abd3794f6246d05c5c19e83d3ccded8afa6d
uygurberkay <50241715+uygurberkay@users.noreply.github.com>
2023-01-11 22:49:33 +0300

    React-router-dom-updated

--- techsup-web-service/src/App.js
+++ techsup-web-service/src/App.js
@@ -2,17 +2,20 @@ import './App.css';
 import React from 'react';
 import Header from './Header';
 import Home from './Home'
+// import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+
 
 function App() {
   return (
-    // BEM  (Come Alive)
-    <div className="App">
-    {/* Header render imported above */}
-    <Header /> 
-    {/* Home */}
-    <Home />
-    </div>
-  );
+      /*BEM  (Come Alive)*/
+        <div className="App">
+          {/* Header render imported above */}
+          <Header /> 
+          {/* Home */}
+          <Home />
+        </div>
+      );
 }
 
 export default App;
