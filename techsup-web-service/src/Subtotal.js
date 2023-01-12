2639df5b7d32f3f762e7fb919432f44560c5177b
uygurberkay <50241715+uygurberkay@users.noreply.github.com>
2023-01-12 17:31:17 +0300

    react-currency-format-updated

--- techsup-web-service/src/Subtotal.js
+++ techsup-web-service/src/Subtotal.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import './Subtotal.css'
+import CurrencyFormat from 'react-currency-format';
+
 
 function Subtotal() {
   return (
     <div className='subtotal'>
-    Subtotal
+        <CurrencyFormat 
+            renderText = {(value)=> (
+                <>
+                    <p>
+                        Subtotal (0 items): <strong>0</strong>
+                    </p>
+                    <small className='subtotal__gift'>
+                        <input type={'checkbox'}/> This order contains a gift
+                    </small>
+                </>
+            )}
+            decimalScale= {2}
+            value = {0}
+            displayType = {'text'}
+            thousandSeperator = {true}
+            prefix={'₺'}
+        />
+
     </div>
   )
 }
