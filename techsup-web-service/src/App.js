7a6b518de58290b717d3e18f91b56201698a5b61
uygurberkay <50241715+uygurberkay@users.noreply.github.com>
2023-01-14 20:12:14 +0300

    14.01.2023

--- techsup-web-service/src/App.js
+++ techsup-web-service/src/App.js
@@ -4,45 +4,39 @@ import Header from './Header';
 import Home from './Home'
 import Login from './Login'
 import Checkout from './Checkout';
-import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch , withRouter} from 'react-router-dom';
 
 function App() {
 
+return (
+      <Router>
+        {/*BEM  (Come Alive)*/}
+        <div className="App">
+          <Switch>
 
-  return (
-    <Router>
-      {/*BEM  (Come Alive)*/}
-      <div className="App">
+            <Route path="/login">
+              {/* Login */}
+              <Login />
+            </Route>
 
-        <Switch>
+            <Route path="/checkout">
+              {/* Header render imported above */}
+              <Header />
+              {/* Checkout */}
+              <Checkout />
+            </Route>
 
-          <Route exact path="/">
-            {/* Header render imported above */}
-            <Header />
-            {/* Home */}
-            <Home />
-          </Route>
-
-          <Route path="/login">
-            {/* Login */}
-            <Login />
-          </Route>
-
-          <Route path="/checkout">
-            {/* Header render imported above */}
-            <Header />
-            {/* Checkout */}
-            <Checkout />
-          </Route>
+            <Route exact path="/">
+              {/* Header render imported above */}
+              <Header />
+              {/* Home */}
+              <Home />
+            </Route>
           
-        </Switch>
-        
-
-      </div>
-
-    </Router>
-      
-      );
+          </Switch>
+        </div>
+      </Router>
+    );
 }
 
 export default App;
--- techsup-web-service/src/Login.js
+++ techsup-web-service/src/Login.js
@@ -3,6 +3,8 @@ import './Login.css'
 import { Link, useHistory } from "react-router-dom";
 import { auth } from "./firebase";
 
+
+
 function Login() {
     const history = useHistory();
     const [email,setEmail] = useState('');
